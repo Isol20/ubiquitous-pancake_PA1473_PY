ca1c3fd94ee6b36136a5953264df4ad64ae39e14
Isol20 <103249443+Isol20@users.noreply.github.com>
2022-05-18 11:48:12 +0200

    s
    New

--- project/main.py
+++ project/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env pybricks-micropython
 import sys
+import time
 
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
@@ -9,7 +10,6 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from pybricks.iodevices import Ev3devSensor
-
 
 lift = Motor(Port.A, positive_direction=Direction.COUNTERCLOCKWISE, gears = [12, 36])
 descend = Motor(Port.A, positive_direction=Direction.CLOCKWISE, gears = [12, 36])
@@ -21,7 +21,18 @@ robot = DriveBase(left_motor, right_motor, wheel_diameter=47, axle_track = 120)
 light = ColorSensor(Port.S3) 
 forksensor = TouchSensor(Port.S1)
 sensor = Ev3devSensor(Port.S1)
+Ultrasensor = UltrasonicSensor(Port.S4)
 
+# FÄRGER
+# grön = 12
+# lila = 15
+# röd = 84 fel
+# blå = 13
+# brun = 20
+# gul  70
+# grå = 92
+# svart = 4 
+# vit = 84
 
 def pick_up_pallet():
     while forksensor.pressed() == False:
@@ -60,33 +71,12 @@ def blue_path():
             robot.drive(-30, -35)
         else: 
             robot.drive(-30, 35)
-        
-def main():
-    #while forksensor.pressed() == False:
-        #predetermined_path()
-    #robot.stop()
-    #lift.run_angle(100, 180, then=Stop.HOLD, wait=False)
-    while light.color() != Color.BLUE: 
-        green_path()
-    robot.stop()
-    while forksensor.pressed() == False:
-        blue_path()
-        #ev3.speaker.beep(frequency=500, duration=100)
-    lift.run_angle(100, 180, then=Stop.HOLD, wait=False)
-    robot.stop()
 
-    #descend.run_angle(5, 70, then=Stop.HOLD, wait=False)
-
-    return 0
-
-#if _name_ == '_main_':
-#    sys.exit(main())
-
-def drive(linecolor): # Follow green line (predetermined path)
-    threshold = (linecolor + 92) / 2
+def drive(linecolor): # Follow line (predetermined path)
+    threshold = (linecolor + 84) / 2
     DRIVE_SPEED =-30
     PROPORTIONAL_GAIN = 1.2
-    while light.color() != Color.BROWN:
+    while True:
         # Calculate the deviation from the threshold.
         deviation = light.reflection() - threshold
 
@@ -101,65 +91,56 @@ def drive(linecolor): # Follow green line (predetermined path)
         # You can wait for a short time or do other things in this loop.
         wait(10)
     robot.stop()
-# Blå = 14-15
-# Röd = 89-90
 
-# grön = 12
-# lila = 15
-# röd = 84
-# blå = 13
-# brun = 20
-# gul  70
-# grå = 9
-# svart = 4 
-# vit = 84
-def from_Purple_2_green():
-    linecolor=15
+def from_green_2_blue():
+    linecolor=1
     threshold = (linecolor + 84) / 2
     DRIVE_SPEED =-50
     PROPORTIONAL_GAIN = 1.2
-    while light.color() != Color.BROWN:#Kör lila
+    while light.color() != Color.BROWN:#Kör grön
+        deviation = light.reflection() - threshold
+        turn_rate = PROPORTIONAL_GAIN * deviation
+        robot.drive(DRIVE_SPEED, turn_rate)
+        wait(10)
+    countdown_turn(3)
+    linecolor=20 #kör brun 
+    threshold = (linecolor + 84) / 2
+    #while light.rgb() != (10, 18, 27): 
+    while light.reflection() != 13:
+        deviation = light.reflection() - threshold
+        turn_rate = PROPORTIONAL_GAIN * deviation
+        robot.drive(DRIVE_SPEED, -turn_rate)
+        wait(10)
+    print("OUT")
+    countdown_turn(2)
+    linecolor=13 #kör blå
+    threshold = (linecolor + 84) / 2
+    while light.color() != Color.BLACK: 
         deviation = light.reflection() - threshold
         turn_rate = PROPORTIONAL_GAIN * deviation
         robot.drive(DRIVE_SPEED, -turn_rate)
         wait(10)
     robot.stop()
-    linecolor=20 #kör brun 
-    threshold = (linecolor + 84) / 2
-    while light.reflection() != 12: 
-        deviation = light.reflection() - threshold
-        turn_rate = PROPORTIONAL_GAIN * deviation
-        robot.drive(DRIVE_SPEED, turn_rate)
-        wait(10)
-    robot.stop()
-    linecolor=12 #kör grön
-    threshold = (linecolor + 84) / 2
-
 
 def from_Purple_2_blue():
-    linecolor=15
+    linecolor=12
     threshold = (linecolor + 84) / 2
     DRIVE_SPEED =-50
     PROPORTIONAL_GAIN = 1.2
     while light.color() != Color.BROWN:#Kör lila
         deviation = light.reflection() - threshold
         turn_rate = PROPORTIONAL_GAIN * deviation
-        robot.drive(DRIVE_SPEED, -turn_rate)
+        robot.drive(DRIVE_SPEED, turn_rate)
         wait(10)
     robot.stop()
     linecolor=20 #kör brun 
     threshold = (linecolor + 84) / 2
-    n=0
-    while light.reflection() != 12: 
+    while light.reflection() != 13: 
         deviation = light.reflection() - threshold
         turn_rate = PROPORTIONAL_GAIN * deviation
-        if n>20:
-            turn_rate=-(PROPORTIONAL_GAIN * deviation)
-        robot.drive(DRIVE_SPEED, turn_rate)
         wait(10)
-        n=+1
     robot.stop()
-    linecolor=12 #kör grön
+    linecolor=13 #kör blå
     threshold = (linecolor + 84) / 2
     while True: 
         deviation = light.reflection() - threshold
@@ -167,21 +148,40 @@ def from_Purple_2_blue():
         robot.drive(DRIVE_SPEED, turn_rate)
         wait(10)
 
-
-def pickup_pallet_new(linecolor):
+def pickup_pallet_grey(linecolor):
     threshold = (linecolor + 9) / 2
     DRIVE_SPEED =-70
     PROPORTIONAL_GAIN = 1.2
-    while forksensor.pressed() == False:
-        deviation = light.reflection() - threshold
-        turn_rate = PROPORTIONAL_GAIN * deviation
-        robot.drive(DRIVE_SPEED, -turn_rate)
-        wait(10)
-    robot.stop()
-    lift.reset_angle(0)
-    lift.run_angle(80,-180, then=Stop.HOLD, wait=True)
-pickup_pallet_new(70)
+    #while countdown(10) > 0:
+    while True:
+        while forksensor.pressed() == False:
+            deviation = light.reflection() - threshold
+            turn_rate = PROPORTIONAL_GAIN * deviation
+            robot.drive(DRIVE_SPEED, -turn_rate)
+            wait(10)
+        robot.stop()
+        lift.reset_angle(0)
+        lift.run_angle(200,180, then=Stop.HOLD, wait=True)
 
-#print(light.reflection())
-#from_Purple_2_green()
-#funk()
+def countdown_turn(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        robot.drive(-20, 180)
+        t -= 1
+
+def main():
+    #drive(13) #Följ blå
+    #drive(20) #Följ lila
+    #pickup_pallet_grey(70)
+    #from_Purple_2_blue()
+    from_green_2_blue()
+    pickup_pallet_grey(70)
+    #pickup_pallet_new(70)
+    #print(light.rgb())
+    return 0
+
+if _name_ == '_main_':
+    sys.exit(main())
